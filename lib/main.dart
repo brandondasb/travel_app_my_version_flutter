5accf26e7abe7937b8af4084d1b7759f2f522d9d
brandon.milambo <brandon.milambo@autotrader.co.uk>
2022-01-06 21:16:13 +0000

    added intro view and custom Dialog using named routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:travel_app_my_version/home_widget.dart';
+import 'package:travel_app_my_version/views/first_view.dart';
+
+import 'home_widget.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,7 +16,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Travel Budget test',
       theme: ThemeData(primarySwatch: Colors.green),
-      home: Home(),
+      // home: Home(),
+      home: FirstView(),
+      routes: <String, WidgetBuilder>{
+        '/signup': (BuildContext context) => Home(),
+        '/home': (BuildContext context) => Home()
+      },
     );
   }
 }
